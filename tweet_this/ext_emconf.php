2ca27f5f300186c3139807122515308e8cc7d4d3
etobi.de <etobi.de@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-19 16:42:00 +0000

    update ext_emconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tweet_this/trunk@26727 735d13b6-9817-0410-8766-e36946ffe9aa

--- tweet_this/ext_emconf.php
+++ tweet_this/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tweet_this".
 #
-# Auto generated 19-11-2009 11:26
+# Auto generated 19-11-2009 17:40
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Tweet This',
-	'description' => 'Twitter from tt_news or t3blog records easily',
+	'description' => 'Send tweets from tt_news or t3blog records easily',
 	'category' => 'be',
 	'author' => 'Tobias Liebig',
 	'author_email' => 'liebig@networkteam.com',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"b5ed";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"a8b3";s:12:"ext_icon.gif";s:4:"0a69";s:14:"ext_tables.php";s:4:"9bad";s:14:"ext_tables.sql";s:4:"693c";s:28:"icon_tx_tweetthis_tweets.gif";s:4:"0a69";s:16:"locallang_db.xml";s:4:"1c49";s:7:"tca.php";s:4:"3172";s:42:"classes/class.tx_tweetthis_AjaxHandler.php";s:4:"4540";s:37:"classes/class.tx_tweetthis_Helper.php";s:4:"3dcd";s:40:"classes/class.tx_tweetthis_userField.php";s:4:"117f";s:19:"doc/wizard_form.dat";s:4:"c153";s:20:"doc/wizard_form.html";s:4:"5300";s:28:"res/userfield_tweetthis.html";s:4:"e6b0";s:16:"static/setup.txt";s:4:"0b45";}',
+	'suggests' => array(
+	),
 );
 
 ?>
