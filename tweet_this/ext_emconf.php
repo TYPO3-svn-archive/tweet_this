a80910dee2228f00ccb3c53b228b0f2d3a1ef170
etobi.de <etobi.de@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-22 15:19:30 +0000

    * first release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tweet_this/trunk@26800 735d13b6-9817-0410-8766-e36946ffe9aa

--- tweet_this/ext_emconf.php
+++ tweet_this/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tweet_this".
 #
-# Auto generated 19-11-2009 17:40
+# Auto generated 22-11-2009 16:18
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.3',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"b5ed";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"a8b3";s:12:"ext_icon.gif";s:4:"0a69";s:14:"ext_tables.php";s:4:"9bad";s:14:"ext_tables.sql";s:4:"693c";s:28:"icon_tx_tweetthis_tweets.gif";s:4:"0a69";s:16:"locallang_db.xml";s:4:"1c49";s:7:"tca.php";s:4:"3172";s:42:"classes/class.tx_tweetthis_AjaxHandler.php";s:4:"4540";s:37:"classes/class.tx_tweetthis_Helper.php";s:4:"3dcd";s:40:"classes/class.tx_tweetthis_userField.php";s:4:"117f";s:19:"doc/wizard_form.dat";s:4:"c153";s:20:"doc/wizard_form.html";s:4:"5300";s:28:"res/userfield_tweetthis.html";s:4:"e6b0";s:16:"static/setup.txt";s:4:"0b45";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"b5ed";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"1364";s:12:"ext_icon.gif";s:4:"0a69";s:14:"ext_tables.php";s:4:"ef87";s:14:"ext_tables.sql";s:4:"6d5f";s:28:"icon_tx_tweetthis_tweets.gif";s:4:"0a69";s:16:"locallang_db.xml";s:4:"e4d3";s:7:"tca.php";s:4:"3172";s:42:"classes/class.tx_tweetthis_AjaxHandler.php";s:4:"5d38";s:37:"classes/class.tx_tweetthis_Helper.php";s:4:"9c63";s:40:"classes/class.tx_tweetthis_userField.php";s:4:"ac3a";s:19:"doc/wizard_form.dat";s:4:"c153";s:20:"doc/wizard_form.html";s:4:"5300";s:28:"res/userfield_tweetthis.html";s:4:"e6b0";s:16:"static/setup.txt";s:4:"0b45";}',
 	'suggests' => array(
 	),
 );
